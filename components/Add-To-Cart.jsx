7c6214788fd350338cafea016c3e3ecca9ca5855
Willyam Ramos <ramoswillyam1@gmail.com>
2023-01-01 15:50:12 -0800

    Made select option value take in the object of option which was parsed and stringified in JSON in order to put it into a piece of state containing both the id and image url of the variant selected

--- components/Add-To-Cart.jsx
+++ components/Add-To-Cart.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 
-const AddToCart = ({ variantId, buttonText, options = false }) => {
-    const [id, setId] = useState(variantId);
+const AddToCart = ({ variantId, buttonText, options = false, changePhoto }) => {
+    const [selectedOption, setSelectedOption] = useState({ id: variantId, imgSrc: '' })
 
     const handleChange = (e) => {
-        setId(e.target.value);
+        let option = JSON.parse(e.target.value)
+        setSelectedOption({ id: option.id, imgSrc: option.image.url })
+        changePhoto(option.image.url)
     }
 
     const addToCart = async () => {
@@ -17,7 +19,7 @@ const AddToCart = ({ variantId, buttonText, options = false }) => {
 
         const result = await fetch('/api/add-to-cart', {
             method: 'POST',
-            body: JSON.stringify({ cartId: localCartData.cartId, variantId: id })
+            body: JSON.stringify({ cartId: localCartData.cartId, variantId: selectedOption.id })
         });
 
         if (!result.ok) {
@@ -30,16 +32,19 @@ const AddToCart = ({ variantId, buttonText, options = false }) => {
 
     return (
         <>
-            {options.length > 1 && (
-                <select name='variant' className='options' onChange={handleChange}>
-                    {options.map((option) => (
-                        <option value={option.node.id} key={option.node.id}>
-                            {option.node.title}
-                        </option>
-                    ))}
-                </select>
-            )}
-            <button className="button" onClick={addToCart}>
+            <div className='flex flex-col gap-2'>
+                <label htmlFor="variant" className='text-black font-extrabold'>Flavors</label>
+                {options.length > 1 && (
+                    <select name='variant' className='bg-slate-800/30 w-4/5 p-1' onChange={handleChange}>
+                        {options.map((option) => (
+                            <option value={JSON.stringify(option.node)} key={option.node.id}>
+                                {option.node.title}
+                            </option>
+                        ))}
+                    </select>
+                )}
+            </div>
+            <button className="button w-4/5" onClick={addToCart}>
                 {buttonText}
             </button>
         </>
