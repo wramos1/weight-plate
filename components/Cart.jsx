87478a64ca52a1a516ad52949ea1224319b8b352
Willyam Ramos <ramoswillyam1@gmail.com>
2023-01-03 09:56:00 -0800

    Implemented the deleteItems function which takes in the cartId and lineId to delete the item from the cart

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -3,7 +3,6 @@ import Image from 'next/image';
 
 const Cart = () => {
     const [cart, setCart] = useState({ id: null, lines: [], estimatedCost: 0, items: 0 });
-    const [open, setOpen] = useState(false);
 
     const getCart = async () => {
         let localCartData = JSON.parse(window.localStorage.getItem('Weight-Plate:cart'));
@@ -50,7 +49,6 @@ const Cart = () => {
             const state = window.localStorage.getItem('Weight-Plate:cart:status');
             if (state && state === 'dirty') {
                 getCart();
-                setOpen(true);
                 window.localStorage.setItem('Weight-Plate:cart:status', 'clean');
             };
         }, 500)
@@ -58,16 +56,29 @@ const Cart = () => {
         return () => {
             clearInterval(interval);
         };
-    }, [])
-
-
-    const toggleCart = () => {
-        setOpen(!open)
-    };
+    }, [cart.lines])
 
     const emptyCart = () => {
         window.localStorage.removeItem('Weight-Plate:cart');
         window.localStorage.setItem('Weight-Plate:cart:status', 'dirty');
+    }
+
+    const deleteItems = async (itemId) => {
+        if (window.confirm('Are you sure you wish to delete this item?')) {
+            const localCartData = JSON.parse(window.localStorage.getItem('Weight-Plate:cart'));
+            if (!localCartData.cartId) {
+                console.error('Error loading your cart');
+                return;
+            }
+            const result = await fetch('/api/remove-from-cart', {
+                method: 'POST',
+                body: JSON.stringify({ cartId: localCartData.cartId, lineIds: itemId })
+            })
+
+            if (!result.ok) {
+                console.error('There was a problem removing the item to the cart');
+            }
+        }
     }
 
     const formattedPrice = new Intl.NumberFormat('en-US', {
@@ -76,45 +87,41 @@ const Cart = () => {
     });
 
     return (
-        <div className="cart">
-            <button className="icon" onClick={toggleCart}>
-                <Image
-                    src="https://www.freeiconspng.com/thumbs/cart-icon/basket-cart-icon-27.png"
-                    alt=""
-                    width={50}
-                    height={50}
-                />
-            </button>
+        <div>
+            <h3 className='title'>
+                Your Cart
+            </h3>
+            {cart.lines.length > 0 ? (
+                <>
+                    <ul>
+                        {
+                            cart.lines.map(({ node: item }, num) => (
+                                <li key={num}>
+                                    <p>
+                                        {item.quantity} &times; {item.merchandise?.product?.title} {item.merchandise?.title === 'Default Title' ? '' : `- ${item.merchandise?.title}`}
+                                    </p>
+                                    <button className="formal-button bg-red-400" onClick={() => deleteItems(item.id)}>
+                                        Delete
+                                    </button>
+                                </li>
+                            ))
+                        }
+                        <li className="total">
+                            <p>Total: {cart.estimatedCost === 0 ? 'FREE' : `${formattedPrice.format(cart.estimatedCost)}`}</p>
+                        </li>
+                    </ul>
 
-            <div>
-                <h3>Your Cart</h3>
-                {cart.lines.length > 0 ? (
-                    <>
-                        <ul>
-                            {
-                                cart.lines.map(({ node: item }, num) => (
-                                    <li key={num}>
-                                        <p>
-                                            {item.quantity} &times; {item.merchandise?.product?.title} {item.merchandise?.title === 'Default Title' ? '' : `- ${item.merchandise?.title}`}
-                                        </p>
-                                    </li>
-                                ))
-                            }
-                            <li className="total">
-                                <p>Total: {cart.estimatedCost === 0 ? 'FREE' : `${formattedPrice.format(cart.estimatedCost)}`}</p>
-                            </li>
-                        </ul>
-                        <a href={`${cart.checkoutUrl}`} className="button">
-                            Check Out
-                        </a>
-                        <button className="empty-cart" onClick={emptyCart}>
-                            Empty Cart
-                        </button>
-                    </>
-                ) : (
-                    <p>Your cart is empty</p>
-                )}
-            </div>
+                    <a href={`${cart.checkoutUrl}`} className="button">
+                        Check Out
+                    </a>
+
+                    <button className="button" onClick={emptyCart}>
+                        Empty Cart
+                    </button>
+                </>
+            ) : (
+                <p>Your cart is empty</p>
+            )}
         </div >
     )
 }
