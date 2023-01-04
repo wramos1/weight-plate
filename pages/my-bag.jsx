459728645cc50a197a28bc4eeab5617f0bb37b7f
Willyam Ramos <ramoswillyam1@gmail.com>
2023-01-03 19:16:37 -0800

    Renamed checkout page to my-bag page to avoid confusion when going to shopify checkout page

---
+++ pages/my-bag.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cart from '../components/Cart'
+
+const MyBag = () => {
+    return (
+        <>
+            <Cart />
+        </>
+    )
+}
+
+export default MyBag;
